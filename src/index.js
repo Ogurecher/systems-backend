2ddaf1488f549e321490a83ecd039db6c9c41c0e
Dmitrii <ddm157@yandex.ru>
2021-01-10 20:07:59 +0300

    Added express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const { ApolloServer } = require('apollo-server');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 
 const mockData = JSON.parse(fs.readFileSync(path.join(__dirname, 'mock-data.json'), 'utf8'));
 
@@ -16,8 +17,10 @@ const server = new ApolloServer({
     resolvers
 });
 
-server
-    .listen()
-    .then(({ url }) =>
-        console.log(`Server is running on ${url}`)
-    );
+const app = express();
+
+app.use('/', express.static('public'));
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => console.log(`GraphQL server ready at http://localhost:4000${server.graphqlPath}`));
